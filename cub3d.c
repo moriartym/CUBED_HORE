6a69d06fbce26e6e4e569ca593232e6f89bc7f9d
darrenfasius <darrenfasiuslim@gmail.com>
2025-05-23 23:02:44 +0800

    cub3d

--- cub3d.c
+++ cub3d.c
@@ -38,8 +38,8 @@ int main(int argc, char **argv)
 		return (ft_putstr_fd("./cub3d map.cub\n", 1), 1);
 	map = (t_map){0};
 	map.name = argv[1];
-	// map.name = "map/valid.cub";
-	map.name = "map/test.cub";
+	map.name = "map/valid.cub";
+	// map.name = "map/test.cub";
 	extract_map(&map);
 	create_visual(&map);
 }
